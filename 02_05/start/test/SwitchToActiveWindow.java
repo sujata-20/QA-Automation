a1f40530565943ab59e3ca76d0279cc03ccf5ace
Sujata Mohapatra <65625033+sujatagit@users.noreply.github.com>
2020-06-07 09:17:35 -0500

    Add files via upload

---
+++ 02_05/start/test/SwitchToActiveWindow.java
@@ -0,0 +1,27 @@
+import org.apache.xpath.objects.XString;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SwitchToActiveWindow {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/sujatamohapatra/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/switch-window");
+        WebElement newTabButton = driver.findElement(By.id("new-tab-button"));
+        newTabButton.click();
+
+        String originalHandle = driver.getWindowHandle();
+        for (String handle1:driver.getWindowHandles()){
+            driver.switchTo().window(originalHandle);
+        }
+
+
+
+        driver.quit();
+    }
+}
