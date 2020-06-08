c5de3951cfbf50cc8a3a68b2c40e8fb5b8428a7a
Sujata Mohapatra <65625033+sujatagit@users.noreply.github.com>
2020-06-08 10:06:46 -0500

    Add files via upload

---
+++ 04_03/start/test/Datepicker.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Datepicker {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/sujatamohapatra/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/datepicker");
+
+        WebElement datePicker = driver.findElement(By.id("datepicker"));
+        datePicker.sendKeys("06/09/2020");
+        datePicker.sendKeys(Keys.RETURN);
+
+
+
+        driver.quit();
+    }
+}
