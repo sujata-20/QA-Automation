96e9c4bd5c60d936f6de57cf09a74e07b7077d6a
Sujata Mohapatra <65625033+sujatagit@users.noreply.github.com>
2020-06-08 14:31:27 -0500

    Add files via upload

---
+++ 04_05/start/test/FileUpload.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+
+public class FileUpload {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/sujatamohapatra/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/fileupload");
+
+        WebElement fileUpload = driver.findElement(By.id("file-upload-field"));
+        fileUpload.sendKeys("Regression Testing_Quality gate.pdf");
+
+        driver.quit();
+    }
+}
