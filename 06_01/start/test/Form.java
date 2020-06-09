308312620da88d21b5f8e76df9da885d212909d8
Sujata Mohapatra <65625033+sujatagit@users.noreply.github.com>
2020-06-09 10:47:57 -0500

    Add files via upload

---
+++ 06_01/start/test/Form.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Form {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/sujatamohapatra/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+
+        driver.findElement(By.id("first-name")).sendKeys("Sujata");
+        driver.findElement(By.id("last-name")).sendKeys("Mohapatra");
+        driver.findElement(By.id("job-title")).sendKeys("Sr QA Analyst");
+        driver.findElement(By.id("radio-button-3")).click();
+        driver.findElement(By.id("checkbox-2")).click();
+        driver.findElement(By.cssSelector("option[value='4']")).click();
+        driver.findElement(By.id("datepicker")).sendKeys("06/03/2020");
+        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
+
+        driver.findElement(By.xpath("/html/body/div/form/div/div[8]/a")).click();
+
+
+
+
+        driver.quit();
+
+    }
+}
